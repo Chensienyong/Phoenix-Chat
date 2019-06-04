c91113fc7cf070bce8812b1da2d976ac2fc5a28d
Chensienyong <ashxcellent_kingzr@yahoo.com>
2019-06-04 22:57:55 +0700

    prod wrong url

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :chat, ChatWeb.Endpoint,
-  url: [scheme: "https", host: "phxchat.herokuapp.com", port: 443],
+  load_from_system_env: true,
+  url: [scheme: "https", host: "phoexchat.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
